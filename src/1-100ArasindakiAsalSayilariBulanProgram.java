e78f41f53471348dba6ca7ca7019cb7a4ca71ad8
[Ali Yilmaz] <[aliyilmaztr96@gmail.com]>
2023-02-26 22:56:38 +0300

    1-100-Arasindaki-Asal-Sayilari-Bulan-Program

--- src/1-100ArasindakiAsalSayilariBulanProgram.java
+++ src/1-100ArasindakiAsalSayilariBulanProgram.java
@@ -1,10 +1,4 @@
-# 1-100-Arasindaki-Asal-Sayilari-Bulan-Program
-Java 1-100 Arasındaki Asal Sayıları Bulan Program
-
-www.patika.dev
-
 public class Main {
-    
     public static void main(String[] args) {
 
         for (int prime = 2; prime <= 100; prime++) {
